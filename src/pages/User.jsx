024dc5567908399d4167a009f505af1366d8a24a
VTJ-Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2023-08-10 18:35:37 +0200

    Edit profile route

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchUserProfile } from '../actions/profile.action'
+import { fetchUserProfile, updateUserProfile } from '../actions/profile.action'
 
 import EntryItem from '../components/EntryItem'
 
@@ -8,16 +8,68 @@ function User() {
   const dispatch = useDispatch()
   const { profileData } = useSelector(state => state.profile)
 
+  const [editing, setEditing] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
   useEffect(() => {
     document.title = "Argent Bank - My profile"
     dispatch(fetchUserProfile())
   }, [dispatch]);
 
+  const editMode = () => {
+    setFirstName(profileData?.body.firstName);
+    setLastName(profileData?.body.lastName);
+    setEditing(true);
+  };
+
+  const handleSave = () => {
+    const updatedData = {
+      firstName,
+      lastName,
+    };
+
+    dispatch(updateUserProfile(updatedData));
+    setEditing(false);
+  };
+
   return (
     <main className="main bg-dark">
       <div className="header">
-        <h1>Welcome back<br />{profileData?.body.firstName} {profileData?.body.lastName}!</h1>
-        <button className="edit-button">Edit Name</button>
+        <h1>
+          Welcome back<br />
+
+          {editing ? (
+            <div>
+              <input
+                type="text"
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+                required
+              />
+              <input
+                type="text"
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+                required
+              />
+            </div>
+          ) : (
+            profileData ?
+            (`${profileData.body.firstName} ${profileData.body.lastName}!`)
+            : ('––––––––––')
+          )}
+
+        </h1>
+        {editing ? (
+          <button className="edit-button" onClick={handleSave}>
+            Save
+          </button>
+        ) : (
+          <button className="edit-button" onClick={editMode}>
+            Edit Name
+          </button>
+        )}
       </div>
       <h2 className="sr-only">Accounts</h2>
       <section className="account">
